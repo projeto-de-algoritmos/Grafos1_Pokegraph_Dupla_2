24f3bc625dae96186d6592f67f749beca05269e3
Artur Vieira <artur.vieira@singletech.com.br>
2022-11-19 14:34:05 -0300

    terminado front

--- PokeGraph/pokegraph_frontend/src/app/home/pokemon-select/pokemon-select.component.ts
+++ PokeGraph/pokegraph_frontend/src/app/home/pokemon-select/pokemon-select.component.ts
@@ -3,11 +3,9 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {PokemonService} from './pokemon.service';
 import {Pokemon} from './pokemons';
 import {MatSelectChange} from "@angular/material/select";
+import {MatDialog} from "@angular/material/dialog";
+import {ModalWinnerComponent} from "../modal-winner/modal-winner.component";
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
 
 @Component({
   selector: 'app-pokemon-select',
@@ -20,28 +18,42 @@ export class PokemonSelectComponent implements OnInit {
   imageAlt: any;
   imageSourceSelf: any;
   imageSourceOther: any;
-  firstPokemon: any;
-  secondPokemon: any;
+  firstPokemon: any = null;
+  secondPokemon: any = null;
 
   pokemonForm = this.formBuilder.group({
-    firstPokemon: '',
-    secondPokemon: '',
+    pokemon1: '',
+    pokemon2: '',
   });
 
   constructor(
     private pokemonService: PokemonService,
     private formBuilder: FormBuilder,
+    public dialog: MatDialog,
   ) {
 
   }
 
   ngOnInit(): void {
+    console.log(this.firstPokemon)
     this.pokemonService.listPokemon().subscribe((response: any) => {
       response.map((pokemon: Pokemon) => {
         this.extract(pokemon);
       });
       this.options = response
       console.log(this.options)
+    });
+  }
+
+  openDialog(response: any): void {
+    const dialogRef = this.dialog.open(ModalWinnerComponent, {
+      width: '500px',
+      height: '500px',
+      data: response,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
     });
   }
 
@@ -53,19 +65,27 @@ export class PokemonSelectComponent implements OnInit {
     };
   }
 
-  loadPokemonSelf($event: MatSelectChange) {
-    this.imageSourceSelf = $event.value.url
-    this.firstPokemon = $event.value.id
-  }
 
+  loadPokemonSelf($event: MatSelectChange) {
+    if($event.value !== null){
+      this.imageSourceSelf = $event.value.url
+      this.firstPokemon = $event.value.id
+    } else {
+      this.firstPokemon = $event.value
+    }
+  }
   loadPokemonOther($event: MatSelectChange) {
-    this.imageSourceOther = $event.value.url
-    this.secondPokemon = $event.value.id
+    if($event.value !== null){
+      this.imageSourceOther = $event.value.url
+      this.secondPokemon = $event.value.id
+    } else {
+      this.secondPokemon = $event.value
+    }
   }
 
   battle() {
     this.pokemonService.battlePokemon(this.firstPokemon, this.secondPokemon).subscribe((response: any) => {
-      console.log(response)
+      this.openDialog(response);
     });
 
   }
